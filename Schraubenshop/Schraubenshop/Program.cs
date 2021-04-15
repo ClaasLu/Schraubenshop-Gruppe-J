3fa0cf49a4b3a1d8fc691caa8272d296bd9f386d
ClaasLu <claas.luebbers@student.jade-hs.de>
2021-04-15 18:38:16 +0200

    New

---
+++ Schraubenshop/Schraubenshop/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schraubenshop
+{
+    class Program
+    {
+        static void Main()
+        { 
+            while (true)
+            { 
+                // Daten einlesen
+                Console.WriteLine("Kopfart");
+                int kopfart = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Kopfhoehe");
+                double kopfhoehe = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Kopfdurchmesser");
+                double kd = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Gewindedurchmesser");
+                double gd = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Gewindelaenge");
+                double laenge = Convert.ToDouble(Console.ReadLine());
+
+                // neue Schraube anlegen
+               Schraube s = new Schraube(kopfart, kopfhoehe, kd, gd, laenge);
+
+                // Schraubendaten berechnen
+                s.Berechnung();
+
+
+                //Schraubendaten ausgeben
+                s.Ausgabe();
+
+                Console.ReadKey();
+                Console.Clear();
+                
+            }
+        }
+            
+        
+    }
+
+}
+    
+
---
+++ Schraubenshop/Schraubenshop/Schraube.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Schraubenshop
+{
+    class Schraube
+    {
+
+        public int Kopfart { get; set; }
+        public double Kopfhoehe { get; set; }
+        public double Kopfdurchmesser { get; set; }
+        public double Gewindedurchmesser { get; set; }
+        public double Gewindelaenge { get; set; }
+        public double Volumen { get; set; }
+        public double Gewicht { get; set; }
+        public double Preis { get; set; }
+
+        ~Schraube()
+        {
+
+        }
+
+
+        public Schraube(int kopfart, double kopfhoehe, double kopfdurchmesser, double gewindedurchmesser, double gewindelaenge)
+        {
+            Kopfart = kopfart;
+            Kopfhoehe = kopfhoehe;
+            Kopfdurchmesser = kopfdurchmesser;
+            Gewindedurchmesser = gewindedurchmesser;
+            Gewindelaenge = gewindelaenge;
+        }
+
+        public void Berechnung()
+        {
+            this.Volumen = 5;
+            this.Gewicht = 4;
+            this.Preis = 3;
+
+        }
+
+        public void Ausgabe()
+        {
+            Console.WriteLine(Volumen);
+            Console.WriteLine(Gewicht);
+            Console.WriteLine(Preis);
+
+
+        }
+
+
+    }
+}
