b72cb758dc0fb457610223237e65fae931232db0
mcniki <80037033+mcniki@users.noreply.github.com>
2021-05-10 11:31:02 +0200

    update neue "Main" Version

---
+++ devniklas/Schraubenshop/Schraubenshop/Material.cs
@@ -0,0 +1,63 @@
+﻿namespace Schraubenshop
+{
+    class Material
+    {
+        // Konstanten für Dichte und Faktor
+        const double dichteStahl = 0.00000785;
+        const double dichtetitan = 0.0000045;
+        const double dichtemessing = 0.00000896;
+        const double faktorStahl = 1;
+        const double faktorEdelstahl = 2;
+        const double faktorTitan = 4;
+        const double faktorMessing = 1.5;
+        
+
+        public int Materialschraube { get; set; }
+        public double Dichte { get; set; }
+        public double Preisfaktor { get; set; }
+
+        //Konstruktor
+        public Material(int materialschraube)
+        {
+            Materialschraube = materialschraube;
+        }
+
+        //Dichte auswählen
+        public double Dichteauswahl()
+        {
+            if (Materialschraube == 1 | Materialschraube == 2)
+            {
+                this.Dichte = dichteStahl;
+            }
+            else if (Materialschraube == 3)
+            {
+                this.Dichte = dichtetitan;
+            }
+            else
+            {
+                this.Dichte = dichtemessing;
+            }
+            return (Dichte);
+        }
+   
+        //Preisfaktor auswählen
+        public double Preisfaktorauswahl()
+        {
+            switch (Materialschraube)
+            {
+                case 1:
+                    this.Preisfaktor = faktorStahl;
+                    return Preisfaktor;                   
+                case 2:
+                    this.Preisfaktor = faktorEdelstahl;
+                    return Preisfaktor;                    
+                case 3:
+                    this.Preisfaktor = faktorTitan;
+                    return Preisfaktor;                    
+                default:
+                    this.Preisfaktor = faktorMessing;
+                    return Preisfaktor;                    
+            }
+        }
+    }
+}
---
+++ devniklas/Schraubenshop/Schraubenshop/Schraube.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Schraubenshop
+{
+    public class Schraube
+    {
+
+        public int Kopfart { get; set; }
+        public Int32 Kopfhoehe { get; set; }
+        public double Kopfdurchmesser { get; set; }
+        public string Gewindedurchmesser { get; set; }
+        public Int32 Gewindelaenge { get; set; }
+        public double Volumen { get; set; }
+        public double Gewicht { get; set; }
+        public double Preis { get; set; }
+        public double Dichte { get; set; }
+        public double Preisfaktor { get; set; }
+        public string Material { get; set; } //zum auslesen der Combo-Box
+
+        public bool Gewinderichtung { get; set; }
+
+
+        //Konstruktor
+        public Schraube()
+        {
+            
+        }
+
+        // Berechnung Sechskant
+        // public void BerechnungSK()
+        // {
+        //      this.Volumen = (6 * 0.5 * Kopfhoehe * (Kopfdurchmesser / 2)) + (Math.PI * (Gewindedurchmesser / 2) * (Gewindedurchmesser / 2) * Gewindelaenge);
+        //     this.Gewicht = Dichte * Volumen;
+       //   this.Preis = 2 + Preisfaktor * Gewicht;
+       //}
+
+        // Berechnung Zylinderkopf
+       // public void BerechnungZK()
+      //  {
+        //    this.Volumen = Math.PI * Kopfhoehe * (Kopfdurchmesser / 2) + Math.PI * (Gewindedurchmesser / 2) * (Gewindedurchmesser / 2);
+          //  this.Gewicht = Dichte * Volumen;
+            //this.Preis = 2 + Preisfaktor * Gewicht;
+        //}
+
+        // Ausgabe der Schraubendaten
+        public void Ausgabe()
+        {
+            Console.Clear();
+            Console.WriteLine("Sie haben ausgewählt:");
+            if (Kopfart == 1)
+            {
+                Console.WriteLine("Sechskantschraube");
+            }
+            else
+            {
+                Console.WriteLine("Zylinderkopfschraube");
+            }
+
+            
+            switch (Preisfaktor)
+            {
+                case 1:
+                    Console.WriteLine("Material: Stahl (verz.)");
+                    break;
+                case 2:
+                    Console.WriteLine("Material: Edelstahl");
+                    break;
+                case 4:
+                    Console.WriteLine("Material: Titan");
+                    break;
+                default:
+                    Console.WriteLine("Material: Messing");
+                    break;
+            }
+
+            Console.WriteLine("Kopf: h=" + Kopfhoehe + "mm;" + "d=" + Kopfdurchmesser + "mm;");
+            Console.WriteLine("M" + Gewindedurchmesser + "x" + Gewindelaenge);
+            Console.WriteLine();
+            Console.WriteLine("Volumen:" + Volumen + " mm^3");
+            Console.WriteLine("Gewicht:" + Gewicht + " kg");
+            Console.WriteLine("Preis:" + Preis + " Euro");
+            Console.WriteLine();
+        }
+    }
+}
